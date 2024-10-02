07b407efcb70077a46d67efee86d476d9036af14
Tomek Sochacki <zbudujmydompl@gmail.com>
2024-10-02 19:44:25 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ export const Header = () => {
   return (
     <header className={styles.container}>
       <img src="/images/logo.jpg" className={styles.logo} />
-      <h1>Our company</h1>
+      <h1>The best company</h1>
+      <p>Future Collars eShop</p>
     </header>
   );
 };
